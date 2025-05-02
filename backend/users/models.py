1a0c97dd2032ffc42bc09e69316dddb6a3f2c5ea
AustinVillanueva <avillann00@gmail.com>
2025-05-02 09:49:03 +0000

    feat(users): Created users model
    In this commit I created the users app on the backend. I also defined my
    custom users models as well as configured the settings and base urls.

---
+++ backend/users/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have a email.')
+        if not username:
+            raise ValueError('Users must have a username.')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=225)
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
