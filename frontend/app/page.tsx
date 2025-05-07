7c7448751d624921f845860514962401759237cf
AustinVillanueva <avillann00@gmail.com>
2025-05-07 15:08:44 +0000

    feat(home): Created simple home page
    In this commit I created a simple home page with an about and contact
    section.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,103 +1,80 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import { useRouter } from 'next/navigation'
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+export default function Home(){
+  const router = useRouter() 
+
+  return(
+    <div className='w-screen min-h-screen bg-blue-50 flex flex-col items-center pt-20'>
+      <nav className='h-16 px-4 fixed top-0 z-50 w-full shadow-md rounded-lg flex flex-row justify-between bg-white items-center'>
+        <image className='w-1/3' />
+        <h1 className='w-1/3 text-black text-center text-2xl'>Contentium</h1>
+        <div className='w-1/3'>
+          <button 
+            className='text-black rounded-lg shadow-md border border-black p-2 bg-yellow-100' 
+            onClick={() => router.push('/login')}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Login
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
+
+      <section className='bg-white p-10 rounded-lg shadow-md max-w-4xl mx-auto text-black text-center'>
+        <h1 className='text-3xl'>About</h1>
+        <p className='mt-5'>
+          This app aims to enable contect creators.
+          It allows you to connect all of your social media accounts so that you can manage them all in one place.
+        </p>
+        <p className='mt-5'>
+          We plan to use machine learning and artificial intelligence in order to help content creators maximize your growth.
+          We will also include features to see how well you are doing with advanced graphs and statistics.
+        </p>
+        <p className='mt-5'>
+          This app is uses Next.js for the front end which is deployed using Vercel.
+          It uses Django for the backend which is deployed using Heroku.
+          For the database, it uses PostgreSQL which is hosted using Supabase.
+        </p>
+      </section>
+
+      <section className='bg-white p-8 rounded-lg shadow-md max-w-4xl mx-auto text-black mt-10'>
+        <h3 className='text-2xl font-semibold mb-6 text-center'>Contact</h3>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-center text-center'>
+          <div>
+            <h5 className='text-lg font-medium'>Email</h5>
+            <a
+              href='mailto:avillann00@gmail.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-600 hover:underline'
+            >
+              avillann00@gmail.com
+            </a>
+          </div>
+          <div>
+            <h5 className='text-lg font-medium'>GitHub</h5>
+            <a
+              href='https://github.com/avillann00'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-600 hover:underline'
+            >
+              GitHub Profile
+            </a>
+          </div>
+          <div>
+            <h5 className='text-lg font-medium'>LinkedIn</h5>
+            <a
+              href='https://www.linkedin.com/in/austin-villanueva-56abbb2b2'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-600 hover:underline'
+            >
+              LinkedIn Profile
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
