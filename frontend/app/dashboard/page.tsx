50740081e80d81c2bff66ad55d961648d98f50a9
AustinVillanueva <avillann00@gmail.com>
2025-05-10 11:30:59 +0000

    feat(dashboard): Basic dashboard
    In this commit I created a basic dashboard with a basic nav bar at the
    top. I also configured the sessions so that users can't access pages
    unless they are logged in.

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,9 +1,33 @@
 'use client'
 
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
 export default function Dashboard(){
+  const { data: session, status } = useSession()
+
+  const router = useRouter()
+
   return(
-    <div className='w-screen h-screen bg-blue-50 flex justify-center'>
-      <h1 className='text-black'>Dashboard</h1>
+    <div className='w-screen h-screen bg-blue-50 flex flex-col'>
+      <div className='text-black flex items-center justify-center h-screen w-screen'>
+        <h1 className='text-2xl'>Dashboard</h1>
+      </div>
+
+      {session ? (
+        <div className='text-black flex items-center justify-center h-screen w-screen'>
+          <h1>Welcome!</h1>
+        </div>
+      ) : (
+        <div className='flex text-black justify-center items-center h-screen w-screen'>
+          <button 
+            onClick={() => router.push('/login')}
+            className='border border-black rounded-lg shadow-md px-6 py-6 bg-white'
+          >
+            Sign in
+          </button>
+        </div>
+      )}
     </div>
   )
 }
