c271e6138cdb27cea8b0d1b4677ff18fe594b1cc
AustinVillanueva <avillann00@gmail.com>
2025-05-11 13:07:35 +0000

    feat(profile): Display more info
    In this commit I configured the profile page to display more info for
    the users.

--- frontend/app/dashboard/profile/page.tsx
+++ frontend/app/dashboard/profile/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 
+import { useSession, signOut } from 'next-auth/react'
+
 export default function Profile(){
+  const { data: session, status } = useSession()
+
   return(
-    <div className='w-screen h-screen bg-blue-50'>
-      <div className='flex justify-center'>
+    <div className='w-screen h-screen bg-blue-50 flex flex-col items-center gap-4'>
         <h1 className='text-black text-3xl mt-15'>Profile</h1>
-      </div>
+
+        <div className='text-black text-center flex flex-col gap-4'>
+          <h1 className='text-xl'>Name</h1>
+          {session?.user.first_name && <h1 className='' >{session?.user.first_name}</h1>}
+          {session?.user.last_name && <h1 className='' >{session?.user.last_name}</h1>}
+          {session?.user.name && <h1 className='' >{session?.user.name}</h1>}
+
+          <h1 className='text-xl'>Email</h1>
+          {session?.user.email && <h1 className='' >{session?.user.email}</h1>}
+
+          {session?.user.username && <h1 className='text-xl'>Username</h1>}
+          {session?.user.username && <h1 className='' >{session?.user.username}</h1>}
+        </div>
+
+        <button 
+          onClick={() => signOut({ callbackUrl: '/' })}
+          className='border border-black rounded-lg shadow-md bg-red-100 text-black py-2 px-2'
+        >
+          Logout
+        </button>
     </div>
   )
 }
