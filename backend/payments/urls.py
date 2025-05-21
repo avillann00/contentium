799a42922158f2b855c6d42d27a3ece6ee88ec27
AustinVillanueva <avillann00@gmail.com>
2025-05-21 13:53:51 +0000

    feat(payments): Finished the payments app
    In this commit I completed the session creation view and its url. I also
    created a webhook and its url so that my backend listens for the
    success. I also changed the user model to have the 'is_pro' field.

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CreateCheckoutSessionView
+from .webhooks import stripe_webhook
 
 urlpatterns = [
-    path('stripe/create-checkout-session/', CreateCheckoutSessionView.as_view(), name='checkout')
+    path('stripe/create-checkout-session/', CreateCheckoutSessionView.as_view(), name='checkout'),
+    path('stripe/webhook/', stripe_webhook, name='stripe-webhook')
 ]
